e19cd1ab70b100987be17b945cbe0a0c80501f1a
Ihsan Varol <ihsan.varol@logo.com.tr>
2015-03-05 10:29:19 +0200

    Hello World

---
+++ com/watchdog/Hello.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package watchdog;
+
+/**
+ * @author IhsanV
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("fdsfsdf");
+	}
+
+}
